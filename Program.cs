c099ffd44046f6022d05cebea84c144cccf7f85a
Mahmood Saadalla <mahmoodsadalla@gmail.com>
2026-03-04 21:06:05 +0200

    Question 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace G_NET_12_OOP03
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace G_NET_12_OOP03
 {
     internal class Program
     {
@@ -11,6 +13,13 @@
             //a) A University has Departments. If the university is closed,
             //the departments no longer exist.
             //the answer : composition
+
+            #endregion
+
+            #region Question 2
+
+            //b) A Driver uses a Car.The driver does not own the car.
+            //the Answer : Association
 
             #endregion
 
