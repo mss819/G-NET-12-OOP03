7c237e043896feb2aac7777c98b732c2ce699cbb
Mahmood Saadalla <mahmoodsadalla@gmail.com>
2026-03-04 21:04:41 +0200

    Question 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 1
+
+            #region Question 1
+
+            //a) A University has Departments. If the university is closed,
+            //the departments no longer exist.
+            //the answer : composition
+
+            #endregion
+
+            #endregion
         }
     }
 }
